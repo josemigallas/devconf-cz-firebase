c06ec4e17dec34257f7c00a1b8470df4eaf5da98
Jose Miguel Gallas <josemigallas@gmail.com>
2017-10-19 17:10:21 +0200

    adds trigger that combines day and room fields in a session

--- functions/index.js
+++ functions/index.js
@@ -15,3 +15,47 @@ exports.duplicateFeedback = functions.database.ref('/votes/{userId}/{sessionId}'
 
         return baseNode.child('sessions').child(sessionId).child('votes').child(userId).set(vote);
     });
+
+// Listens for any change on a session's "day" and created a field called day_room.
+// It's a combined field for easier filtering.
+exports.combineSessionDayAndRoomOnDay = functions.database.ref('/sessions/{sessionId}/day')
+    .onWrite(event => {
+        if (!event.data.exists()) {
+            return;
+        }
+
+        const day = event.data.val();
+
+        const roomRef = event.data.ref.parent.child('room');
+        return roomRef.once("value")
+            .then(snapshot => {
+                const room = snapshot.val();
+
+                console.log(`Day: ${day}, Room: ${room}`)
+
+                return event.data.ref.parent.child("day_room").set(`${day}_${room}`)
+            });
+
+        console.log(`Day: ${day}, Room: ${room}`)
+
+        return event.data.ref.parent.child("day_room").set(`${day}_${room}`)
+    });
+
+exports.combineSessionDayAndRoomOnRoom = functions.database.ref('/sessions/{sessionId}/room')
+    .onWrite(event => {
+        if (!event.data.exists()) {
+            return;
+        }
+
+        const room = event.data.val();
+
+        const dayRef = event.data.ref.parent.child('day');
+        return dayRef.once("value")
+            .then(snapshot => {
+                const day = snapshot.val();
+
+                console.log(`Day: ${day}, Room: ${room}`)
+
+                return event.data.ref.parent.child("day_room").set(`${day}_${room}`)
+            })
+    });
